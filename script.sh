50b31b8a5929898e5bf123fe87dfbfd479cff842
Wen P. Liu <wen.p.liu@gmail.com>
2017-11-19 14:29:48 -0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # install miniconda
-# CarND-Term1-Starter-Kit
+# https://github.com/udacity/CarND-Term1-Starter-Kit/blob/master/doc/configure_via_anaconda.md
 # environment-gpu-tf1.3.yml
 # /usr/local/cuda should have cuda-8/lib64/*, cudnn.h, libcudnn.so.6 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda
