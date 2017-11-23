9140b863f2df2a2f93e77f700f7d429ab991cc2a
Wen Liu <wen.p.liu@gmail.com>
2017-11-22 17:38:57 -0800

    updated script for TF object detection training

--- script.sh
+++ script.sh
@@ -2,6 +2,11 @@
 # install miniconda
 # https://github.com/udacity/CarND-Term1-Starter-Kit/blob/master/doc/configure_via_anaconda.md
 # environment-gpu-tf1.3.yml
-# /usr/local/cuda should have cuda-8/lib64/*, cudnn.h, libcudnn.so.6 
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda
-sudo ldconfig /usr/local/cuda/lib64
+# /usr/local/cuda should have cuda-8/lib64/*, cudnn.h, libcudnn.so.6
+# copy cudnn.h to /cuda
+# ln -s /dependecies/ubuntu16/cudnn/cuda-8.0-xxx/cuda/lib64/libcudnn.so.6 cuda-8.0/lib64
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/devsystem/dependencies/ubuntu16/cuda/cuda-8.0/lib64
+# From tensorflow/models/research/
+protoc object_detection/protos/*.proto --python_out=.
+# From tensorflow/models/research/
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
